911c49d1e4efa5be16ee8214138ad6fd32ab27c0
Dipendra Kushwaha <148966334+Dipu252@users.noreply.github.com>
2024-11-10 15:39:16 +0530

    Add files via upload

---
+++ whatsapp_pyautogui.py
@@ -0,0 +1,8 @@
+import pyautogui as mess
+import time
+
+mess.sleep(3)
+for i in range(100):
+    mess.typewrite("Hello sir!.")
+    mess.sleep(1)
+    mess.press("enter")
---
+++ whatsapp_pywhatkit.py
@@ -0,0 +1,3 @@
+import pywhatkit
+
+pywhatkit.sendwhatmsg("+917060787843", "This is automate message", 15, 56)
